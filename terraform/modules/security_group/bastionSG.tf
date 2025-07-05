5ee55472bb84f42d68613b185bf0e7ace375825a
Ubuntu <ubuntu@boni-prod-bastian-host.asia-south1-c.c.authentic-genre-444610-p2.internal>
2025-07-05 08:09:39 +0000

    updated bh

--- terraform/modules/security_group/bastionSG.tf
+++ terraform/modules/security_group/bastionSG.tf
@@ -9,6 +9,13 @@ resource "aws_security_group" "souq_bastion_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  
+  ingress {
+    from_port   = 27017
+    to_port     = 27017
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
     from_port   = 0
